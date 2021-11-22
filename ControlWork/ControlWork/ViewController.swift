c7f31798cfeeb4764315b106f6b269ae57565240
Nail Galiev <nail.galiev@waveaccess.ru>
2021-11-22 18:15:39 +0300

    [Init] init base proj

---
+++ ControlWork/ControlWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ControlWork
+//
+//  Created by Nail Galiev on 22.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
